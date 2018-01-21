e02f803637914cda90fb79ffb4fd90f9a113026e
German Mosqueda Gonzalez <mogermain@me.com>
2018-01-20 21:12:32 -0600

    seekk

--- crystal-lang.rb
+++ crystal-lang.rb
@@ -14,9 +14,6 @@ class CrystalLang < Formula
   end
 
   bottle do
-    sha256 "fd9e9c2cd01b52a27b85421bd00c2d11fb27bb24b16498d409b1abd666d17ef6" => :high_sierra
-    sha256 "6b76c04f8f6f036b3f3bf4909f73590d6e13251fca1e6a0f3941abc2fbe07686" => :sierra
-    sha256 "c01126afd64bcd1c59e9a6595f2aa4befdcd235a320f87a0dacc3db3813dbfe6" => :el_capitan
   end
 
   head do
